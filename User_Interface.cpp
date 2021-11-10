11cd635cddc02c0df0e68ffa63f0604164d6fb45
Jared Palmer <palmerj27@gator.uhd.edu>
2021-11-10 08:48:27 -0600

    Starting

---
+++ User_Interface.cpp
@@ -0,0 +1,52 @@
+#include "User_Interface.h"
+
+void User_Interface::clearScreen()
+//clears the screen
+{
+    for (int i = 0; i < 40; i++)
+    {
+        cout << endl;
+    }
+}
+
+void User_Interface::displayLogin()
+//starts the login
+{
+
+}
+
+void User_Interface::displayEmployeeMenu()
+//starts the employee menu
+{
+
+}
+
+void User_Interface::displayMainMenu()
+//starts the main menu
+{
+
+}
+
+void User_Interface::displayNewsMenu()
+//starts the news menu
+{
+
+}
+
+void User_Interface::displaySalaryBenefitsMenu()
+//starts the salary benefits menu
+{
+
+}
+
+void User_Interface::displaySchedulingMenu()
+//starts the scheduling menu
+{
+
+}
+
+void User_Interface::display_HR_Menu()
+//starts the HR menu
+{
+
+}
---
+++ User_Interface.h
@@ -0,0 +1,19 @@
+#ifndef USER_INTERFACE_H
+#define USER_INTERFACE_H
+
+
+class User_Interface
+{
+    public:
+        static void clearScreen();
+        static void displayLogin();
+        static void displayMainMenu();
+        static void display_HR_Menu();
+        static void displayNewsMenu();
+        static void displaySchedulingMenu();
+        static void displayEmployeeMenu();
+        static void displaySalaryBenefitsMenu();
+
+};
+
+#endif // USER_INTERFACE_H
