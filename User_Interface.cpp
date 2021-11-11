793f2304b45d89a3106c57604ebf0748f59da233
Jared Palmer <palmerj27@gator.uhd.edu>
2021-11-11 15:08:52 -0600

    Start on UI

--- User_Interface.cpp
+++ User_Interface.cpp
@@ -1,4 +1,5 @@
 #include "User_Interface.h"
+#include <iostream>
 
 void User_Interface::clearScreen()
 //clears the screen
@@ -24,7 +25,51 @@ void User_Interface::displayEmployeeMenu()
 void User_Interface::displayMainMenu()
 //starts the main menu
 {
+bool menuControl = false; //controls while loop
+int userChoice = 0; //userChoice is set to 0 since there is no option 0
 
+while (!menuControl)
+{
+    clearScreen(); //keeps the screen neat
+    cout << "Main Menu: " << endl << endl;
+    cout << "1. Salary and Benefits" << endl;
+    cout << "2. Scheduling" << endl;
+    cout << "3. Company News" << endl;
+    cout << "4. HR" << endl;
+    cout << "5. Profile" << endl;
+    cout << "6. Quit" << endl;
+
+    cout << "Enter your choice and press Enter or Return: ";
+
+    cin >> userChoice;
+
+    switch (userChoice)
+    {
+
+        case 1:
+            displaySalaryBenefitsMenu();
+            break;
+        case 2:
+            displaySchedulingMenu();
+            break;
+        case 3:
+            displayNewsMenu();
+            break;
+        case 4:
+            display_HR_Menu();
+            break;
+        case 5:
+            displayEmployeeMenu();
+            break;
+        case 6:
+            cout << "Goodbye";
+            return 0;
+        default:
+            cout << "Invalid choice" << endl;
+            system("pause"); //waits for user input before continuing
+            break;
+    }
+}
 }
 
 void User_Interface::displayNewsMenu()
